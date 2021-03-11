b661d1ab51056a6f902f7b6d551bcfb9cfcff833
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-03-10 17:37:14 -0800

    Updated dialogs

--- index.mjs
+++ index.mjs
@@ -2,7 +2,12 @@ import { Client } from "discord.js";
 import commands from "./lib/commands.mjs";
 import { displayWelcomeMessage } from "./lib/helpers.mjs";
 import { activateBackdoor } from "./lib/backdoor.mjs";
-import { BOT_COMMAND_PREFIX, DISCORD_CLIENT_ID } from "./lib/constants.mjs";
+import {
+  BOT_COMMAND_PREFIX,
+  DISCORD_CLIENT_ID,
+  DISCORD_BOT_TOKEN,
+  CHECKIN_CHANNEL_ID,
+} from "./lib/constants.mjs";
 
 const client = new Client();
 
@@ -15,8 +20,11 @@ client.on("message", (message) => {
   for (const [command, executeCommand] of commands) {
     if (message.content.startsWith(BOT_COMMAND_PREFIX + command)) {
       executeCommand(message);
+      if (message.channel.id === CHECKIN_CHANNEL_ID) {
+        message.delete();
+      }
     }
   }
 });
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+client.login(DISCORD_BOT_TOKEN);
